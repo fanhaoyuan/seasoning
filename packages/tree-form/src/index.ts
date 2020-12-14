2d3cd5dc823d1ba4d5e62769c856672442562d26
Jarvis <418536538@qq.com>
2020-12-15 00:10:59 +0800

    feat(tree): 新增treeForm类

---
+++ packages/tree-form/src/index.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+import TreeForm from './tree';
+
+export default TreeForm;
---
+++ packages/tree-form/src/tree.ts
@@ -0,0 +1,67 @@
+'use strict';
+
+import { ElementController } from './controllers';
+import { IConfig, IData } from './types';
+import { isString, isObject } from './utils';
+
+export default class Tree {
+    config!: IConfig;
+    destoryed: boolean = false;
+    constructor(config: Partial<IConfig>) {
+        this.initConfig(config);
+        this.set('elementController', new ElementController(this));
+    };
+
+    /**
+     * @description 获取config中的值
+     * @param {string} key config中的键
+     * @returns {unknown} 对应key的值 
+     */
+    get<T extends keyof IConfig>(key: T): IConfig[T] {
+        return this.config[key];
+    };
+
+    /**
+     * @description 设置config中的值
+     * @param {string | object} key config中的键 
+     * @param {any} value 值
+     * @returns {this} 
+     */
+    set<T extends keyof IConfig>(key: T | { [K in T]?: IConfig[K] }, value?: IConfig[T]): this {
+        if (isString(key)) this.config[key] = value;
+        if (isObject(key)) Object.assign(this.config, key);
+        return this;
+    };
+
+    /**
+     * @description 获取默认配置
+     */
+    getDefaultConfig(): IConfig {
+        return {
+
+        };
+    };
+
+    /**
+     * @description 初始化配置项
+     * @param {object} config 配置项 
+     */
+    initConfig(config: Partial<IConfig>) {
+        this.set({ ...this.getDefaultConfig(), ...config })
+    };
+
+    /**
+     * @description 设置数据
+     * @param {object} data
+     */
+    setData(data: IData) {
+        this.set('data', data);
+    };
+
+    /**
+     * @description 渲染函数（调用渲染）
+     */
+    render() {
+
+    }
+};
