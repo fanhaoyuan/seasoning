d49d3fdd0d986efdd6c9d5019f1f3d8ed54045b7
Jarvis <418536538@qq.com>
2020-12-21 00:53:42 +0800

    types(types): 修改配置项

--- packages/tree-form/src/types/interfaces/index.ts
+++ packages/tree-form/src/types/interfaces/index.ts
@@ -1,30 +1,60 @@
 'use strict';
 
-import { ElementController } from "../../controllers";
 import { InputType } from '../constants';
 
 export interface IConfig {
-    /**数据 */
-    data?: IData;
-    /**元素控制器 */
-    elementController?: ElementController;
-    /**输入框类名 */
-    inputClassName: string;
-    /**单选框类名 */
-    radioClassName: string;
-    /**复选框类名 */
-    checkboxClassName: string;
-    /**文字类名 */
-    textClassName: string;
+    /**可嵌套的节点属性的数组，生成 tree 的数据 */
+    data: ITreeNodeConfig[];
+    prefixClass?: string;
+    container: string | HTMLElement;
+    onSelectChange?(): [];
+    onCheckboxChange?(): [];
+    onToggleExpand?(): [];
 }
 
-export interface IData {
-    /**节点key值，唯一标识 */
+export interface ITreeConfig {
+    prefixClass?: string;
+    /**可嵌套的节点属性的数组，生成 tree 的数据 */
+    data: ITreeNodeConfig[];
+
+    treeData: ITreeNodeConfig[];
+
+    defaultExpandedKeys: string[];
+
+    defaultSelectedKeys: string[];
+
+    onCheck?(checkedKeys: string[]): void;
+
+    onExpand?(expandedKeys: string[]): void;
+};
+
+export interface ITreeNodeConfig {
+    /**节点的唯一标识 */
     key: string;
-    /**节点类型 */
-    type: InputType;
-    /**子节点元素 */
-    children: IData[]
-    /**是否为叶子节点 */
-    leaf: boolean;
+    /**标题 */
+    title: string;
+
+    expand?: boolean;
+    /**禁掉响应 默认false*/
+    disabled?: boolean;
+    /**禁掉 checkbox 默认false*/
+    disableCheckbox?: boolean;
+    /**子节点属性数组 */
+    children?: ITreeNodeConfig[];
+
+    checked?: boolean;
+    value?: string;
+    nodeType?: NodeType;
+    hasInput?: boolean;
+    inputOptions: {
+        [x: string]: any
+    }
+};
+
+export type NodeType = 'checkbox' | 'radio' | 'text' | 'input'
+
+export interface ITreeData {
+    title?: string;
+    key: string;
+    children?: ITreeData[]
 }
