ce2a18024d6f20f7037eee375c71d477ef9eb029
Jarvis <418536538@qq.com>
2020-12-21 01:01:45 +0800

    chore: 修改监听事件

--- packages/tree-form/src/components/tree.ts
+++ packages/tree-form/src/components/tree.ts
@@ -9,11 +9,11 @@
   * </div>
   */
 
-import { createElement, addClassName, domTreeRender } from '../utils';
-import { ITreeConfig } from '../types';
+import { createElement, addClassName, domTreeRender, uuid } from '../utils';
+import { ITreeConfig, ITreeNodeConfig } from '../types';
 import TreeNode from './treeNode';
-import { createObserver, IWatcher } from '../observer';
 import eventEmitter from '../eventEmitter';
+import { createObserver } from '../observer';
 
 interface ITreeDOM {
     tree: HTMLDivElement
@@ -21,29 +21,83 @@ interface ITreeDOM {
 
 export default class Tree {
     config: ITreeConfig;
-    constructor(config: Partial<ITreeConfig> = {}) {
-        this.config = Object.assign(this.getDefaultConfig(), config);
+    prefixClass: string = 'tree-form'
+    constructor(config: ITreeConfig) {
+        this.config = createObserver(config, {});
+
+        eventEmitter.on('checkbox:change', (config: ITreeNodeConfig, checked: boolean) => {
+            const data = this.config.data;
+
+            const { key } = config;
+
+            const target = this.findParentNodeByKey(key, data);
+
+            const handle = (children: ITreeNodeConfig[]) => {
+                for (const child of children) {
+                    if (child.nodeType === 'radio') {
+                        child.checked = false;
+                        this.update(child.key, { checked: false })
+                    }
+                };
+            };
+
+            if (checked) {
+                handle(target?.children || []);
+            }
+        });
+
+        eventEmitter.on('radio:change', (config: ITreeNodeConfig, checked: boolean) => {
+            const data = this.config.data;
+            const { key } = config;
+            const target = this.findParentNodeByKey(key, data);
+
+            const handle = (children: ITreeNodeConfig[]) => {
+                for (const child of children) {
+                    if (child.key !== key) {
+                        child.checked = false;
+                        this.update(child.key, { checked: false })
+                    } else {
+                        child.checked = true;
+                        this.update(child.key, { checked: true });
+                    }
+                };
+            };
+
+            handle(target?.children || []);
+        });
+    };
+
+    update(key: string, options: Partial<ITreeNodeConfig>) {
+        eventEmitter.emit(`${key}:update`, options)
+    };
+
+    loop(data: ITreeNodeConfig[], callback: Function) {
+        for (const child of data) {
+            callback(child)
+            this.loop(child?.children || [], callback)
+        };
     }
 
-    getDefaultConfig(): ITreeConfig {
-        return {
-            prefixClass: 'tree-form',
-            data: []
+    findParentNodeByKey(key: string, data: ITreeNodeConfig[]): ITreeNodeConfig | undefined {
+        for (const child of data) {
+            if (child?.children?.some(_ => _.key === key)) {
+                return child;
+            }
+            const res = this.findParentNodeByKey(key, child.children || [])
+            if (res) return res;
         }
     };
 
-    setWatcher(): IWatcher<ITreeConfig> {
-        return {
-
+    findNodeByKey(key: string, data: ITreeNodeConfig[]): ITreeNodeConfig | undefined {
+        for (const child of data) {
+            if (child.key === key) return child;
+            const res = this.findNodeByKey(key, child.children || []);
+            if (res) return res;
         }
-    }
-
-    bindEvents(elements: ITreeDOM) {
-        eventEmitter.on('checkbox:onchange', (e: Event) => console.log('tree', e));
-    }
+    };
 
     createDOM(): ITreeDOM {
-        const { prefixClass } = this.config;
+        const { prefixClass } = this;
 
         const tree = createElement('div', {
             className: prefixClass
@@ -54,25 +108,22 @@ export default class Tree {
 
     createDOMTree(elements: ITreeDOM) {
         const { tree } = elements;
-        const { data, prefixClass } = this.config;
-
+        const { data } = this.config;
         const childrenNode = data.map(childData => {
             return {
-                el: new TreeNode({ ...childData, prefixClass }).render()
+                el: new TreeNode(childData).render()
             };
         });
 
         return {
             el: tree,
             children: childrenNode
-        }
+        };
     };
 
     render(): HTMLElement {
         const elements = this.createDOM();
-        this.bindEvents(elements);
         const domTree = this.createDOMTree(elements);
         return domTreeRender(domTree)
     };
-
 }
